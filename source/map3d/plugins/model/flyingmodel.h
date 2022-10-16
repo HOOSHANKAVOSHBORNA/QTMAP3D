10e2a2a178035aa90b68acb35705d946a22f03e9
client110 <client110@localhost.localdomain>
2022-10-16 17:40:05 +0330

    add hit signal

--- source/map3d/plugins/model/flyingmodel.h
+++ source/map3d/plugins/model/flyingmodel.h
@@ -15,10 +15,14 @@ public:
     void flyTo(const osg::Vec3d& pos, double speed);
     void setPause(bool pause);
     bool getPause() const;
+    void setFollowModel(FlyingModel* followModel);
+    FlyingModel* getFollowModel() const;
 signals:
     void positionChanged(osgEarth::GeoPoint pos);
+    void hit();
 private:
     MapAnimationPathCallback* mAnimationPathCallback;
+    FlyingModel* mFollowModel{nullptr};
 };
 
 #endif // FLYINGMODEL_H
