5774e04c100ddc4093ee97943ab884b95d85fa1a
client111 <client111@localhost.localdomain>
2022-11-26 11:55:27 +0330

    line's plugin modified

---
+++ source/map3d/plugins/drawshapes/drawshapes.h
@@ -0,0 +1,36 @@
+#ifndef GENERICPLUGIN_H
+#define GENERICPLUGIN_H
+
+#include <osgEarthAnnotation/FeatureNode>
+#include <osgEarthSymbology/GeometryFactory>
+#include "plugininterface.h"
+
+class DrawShapes : public PluginInterface
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID PluginInterface_iid FILE  "drawshapes.json")
+    Q_INTERFACES(PluginInterface)
+
+public:
+    explicit DrawShapes(QWidget *parent = nullptr);
+    ~DrawShapes()override{}
+    void setUpUI() override;
+
+
+private:
+    void drawLine();
+    void drawSphere();
+    void drawPolygone();
+    void drawExtrPoly();
+    void drawImageOverlay();
+    void drawCircle();
+    void drawEllipse();
+    void drawRect();
+    osgEarth::Symbology::Geometry* linePath = new osgEarth::Symbology::LineString();
+
+private slots:
+    void onMouseClick(QMouseEvent* event, osgEarth::GeoPoint geoPos);
+    void onMouseMove(QMouseEvent* event, osgEarth::GeoPoint geoPos);
+};
+
+#endif // GENERICPLUGIN_H
