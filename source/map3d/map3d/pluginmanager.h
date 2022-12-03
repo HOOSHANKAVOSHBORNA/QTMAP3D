50f66a7831df3ab9e2e7d9591fcc37a9390a3466
client113 <client113@hooshan.ir>
2022-12-03 14:34:34 +0330

    Removes unused members from ToolboxItemDescProxy

--- source/map3d/map3d/pluginmanager.h
+++ source/map3d/map3d/pluginmanager.h
@@ -17,13 +17,13 @@ class PluginInterface;
 class ToolboxItemDescProxy : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QString name      READ name      WRITE setName      NOTIFY nameChanged     )
-    Q_PROPERTY(QString category  READ category  WRITE setCategory  NOTIFY categoryChanged )
-    Q_PROPERTY(QString iconUrl   READ iconUrl   WRITE setIconUrl   NOTIFY iconUrlChanged  )
-    Q_PROPERTY(bool    checkable READ checkable WRITE setCheckable NOTIFY checkableChanged)
-    Q_PROPERTY(bool    hasMenu   READ hasMenu   WRITE setHasMenu   NOTIFY hasMenuChanged  )
-    Q_PROPERTY(QString menuUrl   READ menuUrl   WRITE setMenuUrl   NOTIFY menuUrlChanged  )
-    Q_PROPERTY(PluginInterface* pluginInterface READ pluginInterface WRITE setPluginInterface NOTIFY pluginInterfaceChanged)
+    Q_PROPERTY(QString name      READ name     )
+    Q_PROPERTY(QString category  READ category )
+    Q_PROPERTY(QString iconUrl   READ iconUrl  )
+    Q_PROPERTY(bool    checkable READ checkable)
+    Q_PROPERTY(bool    hasMenu   READ hasMenu  )
+    Q_PROPERTY(QString menuUrl   READ menuUrl  )
+    Q_PROPERTY(PluginInterface* pluginInterface READ pluginInterface)
 
 
 public:
@@ -46,61 +46,6 @@ public:
     bool    hasMenu  (){return _hasMenu;  }
     QString menuUrl  (){return _menuUrl;  }
     PluginInterface *pluginInterface() { return _pluginInterface; }
-
-signals:
-    void nameChanged     ();
-    void categoryChanged ();
-    void iconUrlChanged  ();
-    void checkableChanged();
-    void hasMenuChanged  ();
-    void menuUrlChanged  ();
-    void pluginInterfaceChanged();
-
-public slots:
-    void setName     (const QString& name) {
-        if (name != _name) {
-            _name = name;
-            emit nameChanged();
-        }
-    }
-    void setCategory (const QString& category) {
-        if (category != _category) {
-            _category = category;
-            emit categoryChanged();
-        }
-    }
-    void setIconUrl  (const QString& iconUrl) {
-        if (iconUrl != _iconUrl) {
-            _iconUrl = iconUrl;
-            emit iconUrlChanged();
-        }
-    }
-    void setCheckable(bool checkable) {
-        if (checkable != _checkable) {
-            _checkable = checkable;
-            emit checkableChanged();
-        }
-    }
-    void setHasMenu  (bool hasMenu) {
-        if (hasMenu != _hasMenu) {
-            _hasMenu = hasMenu;
-            emit hasMenuChanged();
-        }
-    }
-    void setMenuUrl  (const QString& menuUrl) {
-        if (menuUrl != _menuUrl) {
-            _menuUrl = menuUrl;
-            emit menuUrlChanged();
-        }
-    }
-
-    void setPluginInterface(PluginInterface *pluginInterface)
-    {
-        if (pluginInterface != _pluginInterface) {
-            _pluginInterface = pluginInterface;
-            emit pluginInterfaceChanged();
-        }
-    }
 
 private:
     QString _name;
