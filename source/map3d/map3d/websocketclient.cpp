02b45e63d7e462f20c515aed08a03dddc1bfb3f7
client110 <client110@localhost.localdomain>
2022-12-18 12:00:19 +0330

    add key for web socket client and edit debuge messages

--- source/map3d/map3d/websocketclient.cpp
+++ source/map3d/map3d/websocketclient.cpp
@@ -1,16 +1,36 @@
 #include "websocketclient.h"
 
 #include <QDebug>
+#include <QFile>
 #include <QJsonDocument>
+#include <QSslKey>
 #include <QWebSocket>
 
 WebSocketClient::WebSocketClient(QObject *parent) : QObject(parent)
 {
-    mWebSocket = new QWebSocket();
+    mWebSocket = new QWebSocket("Map3d Web Socket Client");
     mWebSocket->setParent(this);
     connect(mWebSocket, &QWebSocket::connected, this, &WebSocketClient::onConnected);
     connect(mWebSocket, QOverload<const QList<QSslError>&>::of(&QWebSocket::sslErrors),
             this, &WebSocketClient::onSslErrors);
+    connect(mWebSocket, &QWebSocket::disconnected, [=](){
+        qDebug() <<tr( "%1 can not connected to %2").arg(mWebSocket->origin()).arg(mWebSocket->requestUrl().toString());
+    });
+
+    QSslConfiguration sslConfiguration;
+//    QFile certFile(QStringLiteral(":/Resources/websocketkey/localhost.cert"));
+    QFile keyFile(QStringLiteral(":/Resources/websocketkey/localhost.key"));
+//    certFile.open(QIODevice::ReadOnly);
+    keyFile.open(QIODevice::ReadOnly);
+//    QSslCertificate certificate(&certFile, QSsl::Pem);
+    QSslKey sslKey(&keyFile, QSsl::Rsa, QSsl::Pem);
+//    certFile.close();
+    keyFile.close();
+    sslConfiguration.setPeerVerifyMode(QSslSocket::VerifyNone);
+//    sslConfiguration.setLocalCertificate(certificate);
+   sslConfiguration.setPrivateKey(sslKey);
+    sslConfiguration.setProtocol(QSsl::TlsV1SslV3);
+    mWebSocket->setSslConfiguration(sslConfiguration);
 
     mWebSocket->open(QUrl(QStringLiteral("wss://localhost:1234")));
 }
@@ -23,7 +43,7 @@ void WebSocketClient::sendMessage(const QJsonDocument &message)
 //! [onConnected]
 void WebSocketClient::onConnected()
 {
-    qDebug() << "WebSocket connected: ";
+    qDebug() <<tr( "%1 connected to %2").arg(mWebSocket->origin()).arg(mWebSocket->requestUrl().toString());
     connect(mWebSocket, &QWebSocket::textMessageReceived,
             this, &WebSocketClient::onTextMessageReceived);
 }
@@ -35,20 +55,24 @@ void WebSocketClient::onTextMessageReceived(QString message)
     //qDebug()<< " Message received:" << message;
     QJsonDocument messageDocument = QJsonDocument::fromJson(message.toUtf8());
     if(messageDocument.isObject())
-        messageReceived(messageDocument);
+        emit messageReceived(messageDocument);
     else
-        qDebug() << "received is not a JSON object";
+        qDebug() << "received messages is not a JSON object";
 
 }
 
 void WebSocketClient::onSslErrors(const QList<QSslError> &errors)
 {
-    Q_UNUSED(errors)
-
+    //Q_UNUSED(errors)
+    qDebug()<<tr("%1 errors:").arg(mWebSocket->origin());
+    for(auto error: errors)
+    {
+        qDebug()<<error.errorString();
+    }
     // WARNING: Never ignore SSL errors in production code.
     // The proper way to handle self-signed certificates is to add a custom root
     // to the CA store.
     //    qDebug() << "mWebSocket error:" << mWebSocket->errorString();
-    mWebSocket->ignoreSslErrors();
+    //mWebSocket->ignoreSslErrors();
 }
 //! [onTextMessageReceived]
