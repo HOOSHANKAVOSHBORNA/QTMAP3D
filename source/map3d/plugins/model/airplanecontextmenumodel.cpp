809a6c39f3abb32744f671545ffd39cc05d59523
client112 <client112@localhost.localdomain>
2022-12-21 17:32:09 +0330

    button text edited

--- source/map3d/plugins/model/airplanecontextmenumodel.cpp
+++ source/map3d/plugins/model/airplanecontextmenumodel.cpp
@@ -12,3 +12,11 @@ int AirplaneContextMenumodel::rowCount(const QModelIndex &parent) const {
 QVariant AirplaneContextMenumodel::data(const QModelIndex &index, int role) const{
     return a_list.at(index.row());
 }
+
+
+QHash<int, QByteArray> AirplaneContextMenumodel::roleNames() const
+{
+    QHash<int, QByteArray> hash = QAbstractListModel::roleNames();
+    hash[TextToShow] = "disp";
+    return hash;
+}
