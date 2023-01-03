1c11b6e37eb5b05080f770c47806a3966571e829
client112 <client112@localhost.localdomain>
2023-01-03 16:18:19 +0330

    station class model and view addede

--- source/map3d/plugins/model/stationtablemodel.h
+++ source/map3d/plugins/model/stationtablemodel.h
@@ -2,18 +2,87 @@
 #define STATIONTABLEMODEL_H
 
 #include <QAbstractTableModel>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QString>
+#include <QStringList>
+#include <deque>
+#include <QSharedPointer>
 
-class StationTableModel : public QAbstractTableModel
+
+struct StationInfo
+{
+    QString Name;
+    QString Type;
+    QString PrimSec;
+
+    double Number;
+    double Latitude;
+    double Longitude;
+
+    QJsonDocument toJson()
+    {
+        QJsonObject jsonObject;
+        jsonObject.insert("Name", Name);
+        jsonObject.insert("Type", Type);
+        jsonObject.insert("Primary/Secondary", PrimSec);
+
+        jsonObject.insert("Number", Number);
+        jsonObject.insert("Latitude", Latitude);
+        jsonObject.insert("Longitude", Longitude);
+
+        QJsonDocument jsonDoc;
+        jsonDoc.setObject(jsonObject);
+        return jsonDoc;
+    }
+
+    void fromJson(QJsonDocument jsonDoc)
+    {
+        QJsonObject data = jsonDoc.object();
+        Name = QString::number(data.value("Name").toInt());
+        Type = data.value("Type").toString();
+        PrimSec = data.value("Primary/Secondary").toString();
+
+        Number = data.value("Number").toDouble();
+        Latitude = data.value("Latitude").toDouble();
+        Longitude = data.value("Longitude").toDouble();
+
+    }
+
+};
+
+class StationTableModel: public QAbstractTableModel
 {
     Q_OBJECT
-
 public:
     StationTableModel(QObject *parent = nullptr);
+
+    enum CustomRoles {
+        BackColorRole = Qt::UserRole + 100,
+        TextColorRole = Qt::UserRole + 101,
+        HeaderTextRole = Qt::UserRole + 102
+    };
+
 
 public:
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QHash<int,QByteArray> roleNames() const override;
+
+    Q_INVOKABLE QString headerText(int column) const;
+    Q_INVOKABLE QString getName(int row) const;
+
+public:
+    void updateItemData(const QString& jsonStr);
+    void updateItemData(const StationInfo& stationInfo);
+
+private:
+    std::deque<QPair<int, QSharedPointer<StationInfo>>> mStationInfoList;
+    std::deque<QPair<int, QSharedPointer<StationInfo>>> mStationInfoListProxy;
+
+    QString mFilter;
 
 };
 
