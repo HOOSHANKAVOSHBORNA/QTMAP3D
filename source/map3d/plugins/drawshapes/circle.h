deef69c421f70cf8f9245262be3c506e9e5abc93
client111 <client111@localhost.localdomain>
2023-01-04 13:22:39 +0330

    shape's editor added to draw shapes

--- source/map3d/plugins/drawshapes/circle.h
+++ source/map3d/plugins/drawshapes/circle.h
@@ -1,17 +1,19 @@
 #ifndef CIRCLE_H
 #define CIRCLE_H
 #include <osgEarthAnnotation/CircleNode>
+#include "mapcontroller.h"
 
 class Circle: public osgEarth::Annotation::CircleNode
 {
 public:
     void setCircleHeight(float hieght);
-    Circle(bool clamp);
+    Circle(MapController *mapController,bool clamp);
     void setColor(osgEarth::Color color);
     void setClamp(bool clamp);
 
 private:
     osgEarth::Symbology::Style circleStyle;
+    MapController* mMapController{nullptr};
 };
 
 #endif // Circle_H
