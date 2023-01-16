b0cff56b79fbc24a8da8eaf63c2c3dfa743f47bd
client113 <client113@localhost.localdomain>
2023-01-16 09:45:07 +0330

    Set MainWindow visible property default value to false

--- source/map3d/map3d/main.cpp
+++ source/map3d/map3d/main.cpp
@@ -6,8 +6,10 @@
 #include <iostream>
 #include <QObject>
 #include <QApplication>
+#include <QDebug>
 
 #include "application.h"
+#include "mainwindow.h"
 
 
 int main(int argc, char *argv[])
@@ -17,10 +19,10 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     Application *const map3DApp = Application::instance();
+    QObject::connect(map3DApp, &Application::mainWindowCreated, [map3DApp]() {
+        map3DApp->mainWindow()->show();
+    });
     map3DApp->initialize();
-
-
-
 
     return app.exec();
 }
