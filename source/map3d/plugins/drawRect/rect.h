e709a5829c29e5b5e668fef321e097fcd85d6e42
client111 <amir@localhost.localdomain>
2023-04-25 11:33:01 +0330

    Rect plugin added

---
+++ source/map3d/plugins/drawRect/rect.h
@@ -0,0 +1,25 @@
+#ifndef RECT_H
+#define RECT_H
+
+
+#include <osgEarthAnnotation/RectangleNode>
+#include "mapcontroller.h"
+
+class Rect : public osgEarth::Annotation::RectangleNode
+{
+public:
+    Rect(MapController *mapController, bool clamp);
+    void setColor(osgEarth::Color color);
+    osgEarth::Color getColor();
+    void setClamp(osgEarth::Symbology::AltitudeSymbol::Clamping clamp);
+    osgEarth::Symbology::AltitudeSymbol::Clamping getClamp();
+    void setStrokeColor(osgEarth::Color color);
+    osgEarth::Color getStrokeColor();
+    void setStrokeWidth(float Width);
+    float getStrokeWidth();
+
+private:
+    MapController* mMapController{nullptr};
+};
+
+#endif // RECT_H
