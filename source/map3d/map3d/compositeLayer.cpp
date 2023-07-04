1dcb1728bfaa6f9bb8f3b91d6cba59ee94539e0f
client112 <client112>
2023-07-04 14:39:00 +0330

    composite layer callbacks added

--- source/map3d/map3d/compositeLayer.cpp
+++ source/map3d/map3d/compositeLayer.cpp
@@ -1,20 +1,69 @@
 #include "compositeLayer.h"
 
-CompositeLayer::CompositeLayer(QObject *parent):
+CompositeAnnotationLayer::CompositeAnnotationLayer(QObject *parent):
     osgEarth::Annotation::AnnotationLayer(),
     QObject(parent)
 {
-    init();
+    addCallback(new CompositeCallback());
 }
 
-osg::Node *CompositeLayer::getNode() const
+osg::Node *CompositeAnnotationLayer::getNode() const
 {
-    _root.get();
+    return _root.get();
 }
 
-void CompositeLayer::init()
+void CompositeAnnotationLayer::init()
 {
     osgEarth::Annotation::AnnotationLayer::init();
 
     _root = new osg::Group();
+}
+
+void CompositeAnnotationLayer::addLayer(osgEarth::Annotation::AnnotationLayer *layer)
+{
+    mChilds.append(layer);
+
+    _root->addChild(layer->getNode());
+}
+
+void CompositeAnnotationLayer::removeLayer(osgEarth::Annotation::AnnotationLayer *layer)
+{
+    mChilds.removeIf([&](osgEarth::Annotation::AnnotationLayer* l){
+        return l == layer;
+    });
+    _root->removeChild(layer->getNode());
+}
+
+//void CompositeAnnotationLayer::addParent(CompositeAnnotationLayer *layer)
+//{
+
+//}
+
+void CompositeAnnotationLayer::fireCallback(CompositeCallback::MethodPtr method)
+{
+    for (CallbackVector::iterator i = _callbacks.begin(); i != _callbacks.end(); ++i)
+    {
+        CompositeCallback* cb = dynamic_cast<CompositeCallback*>(i->get());
+        if (cb) (cb->*method)(this);
+    }
+}
+
+CompositeCallback::CompositeCallback()
+{
+
+}
+
+void CompositeCallback::onVisibleChanged(osgEarth::VisibleLayer *layer)
+{
+
+}
+
+void CompositeCallback::onOpacityChanged(osgEarth::VisibleLayer *layer)
+{
+
+}
+
+void CompositeCallback::onVisibleRangeChanged(osgEarth::VisibleLayer *layer)
+{
+
 }
--- source/map3d/map3d/compositeLayer.h
+++ source/map3d/map3d/compositeLayer.h
@@ -4,20 +4,48 @@
 #include <osgEarthAnnotation/AnnotationLayer>
 
 #include <QObject>
+#include "mapObject.h"
+#include <osgEarth/MapCallback>
+#include <osgEarth/VisibleLayer>
 
-
-
-class CompositeLayer: public QObject, osgEarth::Annotation::AnnotationLayer
+class CompositeCallback : public QObject, public osgEarth::VisibleLayerCallback
 {
     Q_OBJECT
 public:
-    CompositeLayer(QObject *parent = nullptr);
+    CompositeCallback();
+    void onVisibleChanged(osgEarth::VisibleLayer* layer) override;
+    void onOpacityChanged(osgEarth::VisibleLayer* layer) override;
+    void onVisibleRangeChanged(osgEarth::VisibleLayer* layer) override;
+//    void onLayerAdded(osgEarth::Layer* layer, unsigned index) override;
+//    void onLayerRemoved(osgEarth::Layer* layer, unsigned index) override;
+//    void onLayerMoved(osgEarth::Layer* layer, unsigned oldIndex, unsigned newIndex) override;
+//    void onLayerEnabled(osgEarth::Layer* layer) override;
+//    void onLayerDisabled(osgEarth::Layer* layer) override;
+    typedef void (CompositeCallback::*MethodPtr)(class osgEarth::VisibleLayer* layer);
+
+private:
+    MapObject *mMapObject{nullptr};
+};
+
+class CompositeAnnotationLayer: public QObject, osgEarth::Annotation::AnnotationLayer
+{
+    Q_OBJECT
+public:
+    CompositeAnnotationLayer(QObject *parent = nullptr);
 
     virtual osg::Node* getNode() const override;
     virtual void init() override;
 
+public:
+    void addLayer(osgEarth::Annotation::AnnotationLayer *layer);
+    void removeLayer(osgEarth::Annotation::AnnotationLayer *layer);
+    void fireCallback(CompositeCallback::MethodPtr);
+//    void addParent(CompositeAnnotationLayer *layer);
+
 private:
     osg::ref_ptr<osg::Group> _root;
+    QList<osg::ref_ptr<osgEarth::Annotation::AnnotationLayer>> mChilds;
+//    QList<osg::ref_ptr<CompositeAnnotationLayer>> mParents;
 };
 
 #endif // COMPOSITELAYER_H
