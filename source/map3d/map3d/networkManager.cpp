a4d2b2cea9eb830c0af12ca964b3d89699e69229
client110 <HOOSHANKAVOSHBORNA@gmail.com>
2024-02-17 16:36:45 +0330

    set connection network

--- source/map3d/map3d/networkManager.cpp
+++ source/map3d/map3d/networkManager.cpp
@@ -2,42 +2,74 @@
 #include "qamqpexchange.h"
 #include "qamqpqueue.h"
 
-NetworkManager::NetworkManager(QObject *parent): QObject(parent)
-{
-    mClient.setAutoReconnect(true);
-}
-
-void NetworkManager::start()
+NetworkManager::NetworkManager(QObject *parent):
+    QObject(parent)
 {
     connect(&mClient, &QAmqpClient::connected, this, &NetworkManager::clientConnected);
     connect(&mClient, qOverload<QAMQP::Error >(&QAmqpClient::error), this, &NetworkManager::clientError);
-    //    mClient.setHost("192.168.0.110");
-    //    mClient.setPort(5672);
-    //    mClient.setUsername("guest");
-    //    mClient.setPassword("guest");
-    //    mClient.setVirtualHost("/");
-    mClient.connectToHost();
+
+    mClient.setAutoReconnect(true);
+}
+
+//void NetworkManager::start()
+//{
+//    mClient.connectToHost();
+//}
+
+void NetworkManager::setConfig(QString ip, int port, QString username, QString pass)
+{
+//    if(ip != mClient.host()
+//        || port != mClient.port()
+//        || username != mClient.username()
+//        || pass != mClient.password()){
+
+        mClient.disconnectFromHost();
+
+        mClient.setHost(ip);
+        mClient.setPort(port);
+        mClient.setUsername(username);
+        mClient.setPassword(pass);
+        mClient.setVirtualHost("/");
+
+        mClient.connectToHost();
+
+//    }
 }
 
 void NetworkManager::sendMessage(const QString &message)
 {
     QAmqpExchange *exchange = mClient.createExchange();
     exchange->publish(message, "Map3dClient");
-//    qDebug() << "Sent message: "<<message;
+    //    qDebug() << "Sent message: "<<message;
+}
+
+bool NetworkManager::isConnected()
+{
+    return mClient.isConnected();
+}
+
+bool NetworkManager::isConsuming()
+{
+    qDebug()<<"isConsuming: "<<mMap3dQueue->isConsuming();
+    qDebug()<<"Consumer count: "<<mMap3dQueue->consumerCount();
+    qDebug()<<"messageCount count: "<<mMap3dQueue->messageCount();
+    if(mMap3dQueue)
+        return mMap3dQueue->isConsuming();
+    return false;
 }
 
 void NetworkManager::clientConnected()
 {
-    qDebug() << "Client connected.";
-    QAmqpQueue *map3dQueue = mClient.createQueue("Map3d");
+    qDebug() << "Client connected to: "<<mClient.host()<<": "<<mClient.port();
+    mMap3dQueue = mClient.createQueue("Map3d");
     QAmqpQueue *map3dClientQueue = mClient.createQueue("Map3dClient");
-    disconnect(map3dQueue, 0, 0, 0); // in case this is a reconnect
+    disconnect(mMap3dQueue, 0, 0, 0); // in case this is a reconnect
     disconnect(map3dClientQueue, 0, 0, 0);
 
     //--data -------------------------------------------
-    connect(map3dQueue, &QAmqpQueue::declared, this, &NetworkManager::onMap3dQueueDeclare);
+    connect(mMap3dQueue, &QAmqpQueue::declared, this, &NetworkManager::onMap3dQueueDeclare);
     connect(map3dClientQueue, &QAmqpQueue::declared, this, &NetworkManager::onMap3dClientQueueDeclare);
-    map3dQueue->declare();
+    mMap3dQueue->declare();
     map3dClientQueue->declare();
 }
 
