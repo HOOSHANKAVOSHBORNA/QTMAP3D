a9d685a53c162a4d12096b5d5b4e6981c8c40ad9
HOOSHANKAVOSHBORNA <alirezaor73@gmail.com>
2024-03-10 13:12:19 +0330

    save user settings for filter and layer

--- source/map3d/map3d/filterManager.cpp
+++ source/map3d/map3d/filterManager.cpp
@@ -238,12 +238,12 @@ void FilterManager::removeFilterTag(int index)
 {
     if (index >= mFilterTags.size())
         return;
-    mFilterTags.remove(index);
+    mFilterSettings->beginGroup("filter");
     if(mFilterSettings->contains(mFilterTags[index]->field)){
-        mFilterSettings->beginGroup("filter");
         mFilterSettings->remove(mFilterTags[index]->field);
-        mFilterSettings->endGroup();
     }
+    mFilterSettings->endGroup();
+    mFilterTags.remove(index);
     emit filterTagsEdited();
 }
 
